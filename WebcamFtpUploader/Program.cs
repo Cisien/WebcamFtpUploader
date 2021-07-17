c81de8684b3e4263c3f0d4bdcd766176d2799ded
Cisien <ccurwick@exoronet.net>
2021-07-17 14:05:57 -0700

    add rudmentary health check, update to .net 6

--- WebcamFtpUploader/Program.cs
+++ WebcamFtpUploader/Program.cs
@@ -1,54 +1,72 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebcamFtpUploader
 {
     class Program
     {
+        private static readonly string _cameraBaseAddress = Environment.GetEnvironmentVariable("CAMERA_BASE_ADDRESS");
+        private static readonly string _cameraUsername = Environment.GetEnvironmentVariable("CAMERA_USERNAME");
+        private static readonly string _cameraPassword = Environment.GetEnvironmentVariable("CAMERA_PASSWORD");
+
+        private static readonly string _wundergroundDeviceId = Environment.GetEnvironmentVariable("WUNDERGROUND_CAM_ID");
+        private static readonly string _wundergroundDeviceKey = Environment.GetEnvironmentVariable("WUNDERGROUND_CAM_KEY");
+        private static readonly int _uploadInterval = int.Parse(Environment.GetEnvironmentVariable("UPLOAD_INTERVAL_MS"));
+
+        private static readonly Timer _healthcheckTimer = new(HealthCheck);
+        private static DateTimeOffset _lastUpdate = DateTimeOffset.MinValue;
+
         static async Task Main()
         {
-            var cameraBaseAddress = Environment.GetEnvironmentVariable("CAMERA_BASE_ADDRESS");
-            var cameraUsername = Environment.GetEnvironmentVariable("CAMERA_USERNAME");
-            var cameraPassword = Environment.GetEnvironmentVariable("CAMERA_PASSWORD");
-
-            var wundergroundDeviceId = Environment.GetEnvironmentVariable("WUNDERGROUND_CAM_ID");
-            var wundergroundDeviceKey = Environment.GetEnvironmentVariable("WUNDERGROUND_CAM_KEY");
-            var uploadInterval = int.Parse(Environment.GetEnvironmentVariable("UPLOAD_INTERVAL_MS"));
-
+            _healthcheckTimer.Change(TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
             var httpClient = new HttpClient
             {
-                BaseAddress = new Uri($"{cameraBaseAddress}")
+                BaseAddress = new Uri($"{_cameraBaseAddress}")
             };
 
-            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{cameraUsername}:{cameraPassword}")));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_cameraUsername}:{_cameraPassword}")));
 
             while (true)
             {
-                using var imgRequest = await httpClient.GetAsync("/ISAPI/Streaming/channels/101/picture");
-                if(!imgRequest.IsSuccessStatusCode)
+                var cts = new CancellationTokenSource(_uploadInterval / 2);
+                using var imgRequest = await httpClient.GetAsync("/ISAPI/Streaming/channels/101/picture", cts.Token);
+                if (!imgRequest.IsSuccessStatusCode)
                 {
+                    var responseBody = await imgRequest.Content.ReadAsStringAsync(cts.Token);
+                    Console.WriteLine($"The camera returned an error: {imgRequest.StatusCode}: \n\t{responseBody}");
                     await Task.Delay(1000);
                     continue;
                 }
-                using var imgStream = await imgRequest.Content.ReadAsStreamAsync();
+                using var imgStream = await imgRequest.Content.ReadAsStreamAsync(cts.Token);
 
                 var ftp = (FtpWebRequest)WebRequest.Create("ftp://webcam.wunderground.com/image.jpg");
-                ftp.Credentials = new NetworkCredential(wundergroundDeviceId, wundergroundDeviceKey);
+                ftp.Credentials = new NetworkCredential(_wundergroundDeviceId, _wundergroundDeviceKey);
                 ftp.Method = WebRequestMethods.Ftp.UploadFile;
                 ftp.UseBinary = true;
                 ftp.UsePassive = true;
                 ftp.ContentLength = imgRequest.Content.Headers.ContentLength.Value;
 
                 using var ftpRequestStream = await ftp.GetRequestStreamAsync();
-                await imgStream.CopyToAsync(ftpRequestStream);
+                await imgStream.CopyToAsync(ftpRequestStream, cts.Token);
                 using var response = (FtpWebResponse)await ftp.GetResponseAsync();
 
                 Console.WriteLine($"{response.ResponseUri} uploaded: {response.StatusCode}: {response.StatusDescription}");
+                _lastUpdate = DateTimeOffset.UtcNow;
+                await Task.Delay(_uploadInterval);
+            }
+        }
 
-                await Task.Delay(uploadInterval);
+        private static void HealthCheck(object _)
+        {
+            if ((DateTimeOffset.UtcNow - _lastUpdate).TotalMilliseconds > _uploadInterval * 3)
+            {
+                Console.WriteLine($"Upload loop appears to have hung or has failed repeatedly for {_uploadInterval * 3}ms, exiting.");
+                Environment.Exit(1);
             }
         }
     }
